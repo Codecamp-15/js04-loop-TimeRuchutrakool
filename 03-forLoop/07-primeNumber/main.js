0da4620238873146ad7eee34b053f333b9610228
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-08-09 10:29:49 +0700

    improve prime number latest

--- 03-forLoop/07-primeNumber/main.js
+++ 03-forLoop/07-primeNumber/main.js
@@ -1,13 +1,23 @@
 // 1 2 3 5 7 11 13 17 19 23 29 ...
 
+// for (let i = 1; i <= 100; i++) {
+//   let count = 0;
+//   for (let j = 1; j <= 100; j++) {
+//     if (i % j === 0) {
+//       count++;
+//     }
+
+//     if (i === j) break;
+//   }
+//   if (i !== 1 && count <= 2) console.log(i);
+// }
+
 for (let i = 1; i <= 100; i++) {
   let count = 0;
-  for (let j = 1; j <= 100; j++) {
+  for (let j = 1; j <= i; j++) {
     if (i % j === 0) {
       count++;
     }
-
-    if (i === j) break;
   }
-  if (i !== 1 && count <= 2) console.log(i);
+  if (count === 2) console.log(i);
 }
