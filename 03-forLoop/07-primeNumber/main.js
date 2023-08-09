e09dc49af3e5feb21ce0d69df132b082a7dfdbb5
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-08-09 10:31:44 +0700

    prime number super latest

--- 03-forLoop/07-primeNumber/main.js
+++ 03-forLoop/07-primeNumber/main.js
@@ -1,18 +1,8 @@
 // 1 2 3 5 7 11 13 17 19 23 29 ...
 
-// for (let i = 1; i <= 100; i++) {
-//   let count = 0;
-//   for (let j = 1; j <= 100; j++) {
-//     if (i % j === 0) {
-//       count++;
-//     }
+const n = 100;
 
-//     if (i === j) break;
-//   }
-//   if (i !== 1 && count <= 2) console.log(i);
-// }
-
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= n; i++) {
   let count = 0;
   for (let j = 1; j <= i; j++) {
     if (i % j === 0) {
